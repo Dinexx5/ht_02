04df541f9292be978d6fd7793310608ab61a21a8
Dinexx5 <Dinexx298@gmail.com>
2022-12-21 20:50:16 +0300

    now definitely all

--- src/routes/testing-router.ts
+++ src/routes/testing-router.ts
@@ -3,10 +3,10 @@ import {postsRepository} from "../repositories/posts-repository";
 import {blogsRepository} from "../repositories/blogs-repository";
 export const testingRouter = Router({})
 
-testingRouter.delete('/all-data', (req: Request, res: Response) => {
-    let blogs = blogsRepository.getAllBlogs()
+testingRouter.delete('/all-data', async (req: Request, res: Response) => {
+    let blogs = await blogsRepository.getAllBlogs()
     blogs.splice(0,blogs.length)
-    let posts = postsRepository.getAllPosts()
+    let posts = await postsRepository.getAllPosts()
     posts.splice(0,blogs.length)
     res.send(204)
 })
